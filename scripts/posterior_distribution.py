eac483dfa88c1dcb5f19fca704d16771eff7deb9
uhobeike <beike315@icloud.com>
2023-01-04 20:25:34 +0900

    Feature posterior_distribution

---
+++ scripts/posterior_distribution.py
@@ -0,0 +1,73 @@
+# /usr/bin/python3
+
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.axisartist.axislines import SubplotZero
+
+
+def setUniformDistribution(step):
+    return [1/step] * (step+1)
+
+
+def getLikelihood(result, step):
+    if result == "完走":
+        return [1/step * i for i in range(step+1)]
+    elif result == "失敗":
+        return [1-(1/step * i) for i in range(step+1)]
+    else:
+        print("a")
+
+
+def normalize(poster_dist):
+    norm_const = 1/sum(poster_dist)
+    return [(norm_const * i) for i in poster_dist]
+
+
+def getPosteriorDistribution(likelihood, prior_dist):
+    return [l*p for (l, p) in zip(likelihood, prior_dist)]
+
+
+def plot(prob, data, step):
+    fig = plt.figure()
+    ax = SubplotZero(fig, 111)
+    ax.set_xlim(0.0, 1.0)
+    ax.set_ylim(0.0, 0.08)
+    ax.bar(data, prob, width=1/step, edgecolor="black")
+
+    for direction in ["right", "top"]:
+        ax.axis[direction].set_visible(False)
+    for direction in ["left",  "bottom"]:
+        ax.axis[direction].set_axisline_style("-|>")
+    ax.grid(which='major', color='black')
+    ax.grid(which='minor', color='gray', linestyle='--')
+    ax.tick_params(labelsize=30)
+    ax.set_xlabel("完走率", fontname='IPAGothic', fontsize=50)
+    ax.set_ylabel("完走率の確率", fontname='IPAGothic', fontsize=50)
+
+    fig.add_subplot(ax)
+    plt.show()
+
+
+def main():
+    step = 100
+    prior_dist = []
+    poster_dist = []
+    likelihood = []
+
+    prior_dist = setUniformDistribution(step)
+    prob = [1/step * i for i in range(step+1)]
+
+    for result in sys.argv[1:]:
+        likelihood = getLikelihood(result, step)
+        poster_dist = getPosteriorDistribution(likelihood, prior_dist)
+        poster_dist = normalize(poster_dist)
+        prior_dist = poster_dist
+
+    plot(poster_dist, prob, step)
+
+
+if __name__ == '__main__':
+    main()
